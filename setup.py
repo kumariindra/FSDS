34c5138b0e9f54b393d5f36166d9e6db2696639c
kumariindra <kumariindra7@gmail.com>
2022-07-12 20:04:49 +0900

    Check Deployment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from typing import List
 PROJECT_NAME="housing-predictor"
 VERSION="0.0.3"
 AUTHOR="Indra Kumari"
-DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+DESRCIPTION="First FSDS  Machine Learning Project"
 
 REQUIREMENT_FILE_NAME="requirements.txt"
 
